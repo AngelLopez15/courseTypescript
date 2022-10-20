85b9bbb6b0328542f4d5d778311ece2f1698036f
Angel Lopez <alopezcr@aeromexico.com>
2022-10-20 17:52:16 -0500

    clases en javascript

---
+++ 5-clasesJavascript/clases-js6.js
@@ -0,0 +1,23 @@
+// Clase normal de Javascript
+
+class Avenger {
+  name;
+  power;
+
+  constructor(name = 'No name', power = 0) {
+    this.name = name;
+    this.power = power;
+  }
+}
+
+class FlyAvenger extends Avenger {
+  fly;
+
+  constructor(name, power) {
+    super(name, power);
+    this.fly = true;
+  }
+}
+
+const hulk = new Avenger('Hulk', 10000);
+const falcon = new Avenger('Falcon', 200);
